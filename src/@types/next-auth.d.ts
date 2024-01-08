76cf0be5e276262de8f034c458b830c35b2a1f04
Juliano Sill <sill.juliano@gmail.com>
2024-01-08 18:22:35 -0300

    feat: save time intervals to db

--- src/@types/next-auth.d.ts
+++ src/@types/next-auth.d.ts
@@ -4,7 +4,14 @@ declare module 'next-auth' {
   export interface User {
     id: string
     name: string
+    email: string
     username: string
     avatar_url: string
   }
+
+  interface Session {
+    user: User
+  }
+
 }
+
