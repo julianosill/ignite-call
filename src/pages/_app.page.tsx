aef2b9501872cf73112627c0ccd7072e0c99f27d
Juliano Sill <sill.juliano@gmail.com>
2024-01-12 14:20:19 -0300

    feat:(Calendar): update to react-query

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,9 +1,11 @@
 import '../lib/dayjs'
 
 import type { AppProps } from 'next/app'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { SessionProvider } from 'next-auth/react'
 
 import { globalStyles } from '@/styles/global'
+import { queryClient } from '@/lib/react-query'
 
 globalStyles()
 
@@ -12,8 +14,10 @@ export default function App({
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider session={session}>
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
