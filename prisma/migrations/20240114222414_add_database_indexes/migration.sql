29e2ec4d957ce88c9b59a796ebac9368145169f2
Juliano Sill <sill.juliano@gmail.com>
2024-01-14 19:35:09 -0300

    build(prisma): add deploy config for planetscale

---
+++ prisma/migrations/20240114222414_add_database_indexes/migration.sql
@@ -0,0 +1,23 @@
+-- DropForeignKey
+ALTER TABLE `accounts` DROP FOREIGN KEY `accounts_user_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `schedulings` DROP FOREIGN KEY `schedulings_user_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `session` DROP FOREIGN KEY `session_user_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `user_time_intervals` DROP FOREIGN KEY `user_time_intervals_user_id_fkey`;
+
+-- RenameIndex
+ALTER TABLE `accounts` RENAME INDEX `accounts_user_id_fkey` TO `accounts_user_id_idx`;
+
+-- RenameIndex
+ALTER TABLE `schedulings` RENAME INDEX `schedulings_user_id_fkey` TO `schedulings_user_id_idx`;
+
+-- RenameIndex
+ALTER TABLE `session` RENAME INDEX `session_user_id_fkey` TO `session_user_id_idx`;
+
+-- RenameIndex
+ALTER TABLE `user_time_intervals` RENAME INDEX `user_time_intervals_user_id_fkey` TO `user_time_intervals_user_id_idx`;
